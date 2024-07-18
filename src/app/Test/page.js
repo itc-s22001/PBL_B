5ecd5e44292b094999370051d7eac7fbbe6494a7
s22025 <s22025@std.it-college.ac.jp>
2024-07-18 12:18:39 +0900

    試しにエンドポイントつくった

--- src/app/Test/page.js
+++ src/app/Test/page.js
@@ -1,15 +1,29 @@
+"use client";
+
+// backend用
+import axios from "axios";
+
 import s from './page.module.css'
 import Sub from './sub.js'
 
 const Test = () => {
 
 	{/*ここに3年生が色々書きます*/}
-	{/*コメントアウト*/}
+
+	const getData = async ()=> {
+		try {
+			const response = await axios.get('/api');
+			console.log(response)
+		}catch (e) {
+			console.log(e)
+		}
+	}
 
 	return(
 		<>
 			<p className={s.h1}>test</p>
 			<Sub />
+			<button onClick={getData}>BUTTON</button>
 		</>
 	)
 }
