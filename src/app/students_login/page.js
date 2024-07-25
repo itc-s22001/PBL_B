dc2c6059f791b844563bdf6392484845aa6d7c04
itc-s22001 <s22001@std.it-college.ac.jp>
2024-07-25 11:51:15 +0900

    バックエンドをフロントと繋げた。遷移はまだ

--- src/app/students_login/page.js
+++ src/app/students_login/page.js
@@ -1,32 +1,83 @@
+'use client'
+
 import React from 'react';
 import s from './page.module.css';
+import { useEffect, useState } from "react";
+import { signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { auth, provider } from "../firebase";
+import { useRouter } from "next/navigation"; // useRouterの代わりにuseRouterをインポート
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import db from "../firebase"
 
 const StudentsLogin = () => {
 	// ここに3年生が色々書きます
 	// コメントアウト
+	const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter();
+    const [user, setUser] = useState(null);
+
+	useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+        return () => unsubscribe();
+    }, []);
+
+	const handleLogin = async (e) => {
+        e.preventDefault();
+        const auth = getAuth();
+		//メールとパスワードでログイン
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+			if (email === "taro@test.com") {
+				router.push(`/teacher`) //教員でログイン時の遷移先
+			} else {
+				router.push(`/complete_attend`); // ログイン成功後に遷移する
+			}
+        } catch (error) {
+            console.error("エラーです", error);
+			alert('メールかパスワードが間違っています');
+        }
+		// try {
+        //     // グーグルでサインイン
+        //     await signInWithPopup(auth, provider);
+        //     router.push(`/`); // ログイン成功後に遷移する
+        // } catch (error) {
+        //     console.error("エラーです", error);
+        // }
+	}
 
 	return (
-		<>
+		<form onSubmit={handleLogin}>
 			<div className={s.inputControlEmail}>
 				<label htmlFor="FirstName" className="form-label required">メールアドレス</label>
-				<input type="text"  placeholder="メールアドレスを入力してください(半角英数字)" className={s.Forminput}/>
+				<input type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
+						placeholder="メールアドレスを入力してください(半角英数字)" 
+						className={s.Forminput}/>
 				<div className="error"></div>
 			</div>
 
 			<div className={s.inputControlPassword}>
 				<labal htmlFor="FirstName" className="form-label required">　パスワード　</labal>
-				<input type="text" placeholder="パスワードを入力してください(半角英数字)" className={s.Forminput}/>
+				<input type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+						placeholder="パスワードを入力してください(半角英数字)" 
+						className={s.Forminput}/>
 			</div>
 
 			<div>
 				<p className={s.buttonContainer}>
-					<button className={`${s.button} ${s.buttonA}`}>ログイン</button>
+					<button className={`${s.button} ${s.buttonA}`} type="submit">ログイン</button>
 				</p>
 			</div>
-		</>
+		</form>
 	);
 };
 
 // 名前付きエクスポート
-export default StudentsLogin
+export default StudentsLogin;
 
