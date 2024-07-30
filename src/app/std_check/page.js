2cbdd0279f98e34aa740bc3d686328896edab46e
itc-s22021 <s22021@std.it-college.ac.jp>
2024-07-30 09:49:12 +0900

    second commit

--- src/app/std_check/page.js
+++ src/app/std_check/page.js
@@ -3,13 +3,19 @@ import s from './page.module.css';
 const Test = () => {
     return (
       <>
-       <p className={s.h1}>学籍番号：</p>
-       <p className={s.h2}>名前：</p>
-       <p className={s.h2}>授業：</p>
-      <div className={s.buttonContainer}>
-       <button className={`${s.button} ${s.buttonA}`}>出席</button>
-       <button className={`${s.button} ${s.buttonB}`}>欠席</button>
-      </div>
+        <div className={s.container}>
+          <p className={s.h1}>学籍番号：</p>
+          <input type="text" value="123456" readOnly className={s.readOnlyInput} />
+        </div>
+        <div className={s.container}>
+          <p className={s.h2}>名前：</p>
+          <input type="text" value="山田太郎" readOnly className={s.readOnlyInput} />
+        </div>
+        <p className={s.h2}>授業：</p>
+        <div className={s.buttonContainer}>
+          <button className={`${s.button} ${s.buttonA}`}>出席</button>
+          <button className={`${s.button} ${s.buttonB}`}>欠席</button>
+        </div>
       </>
     )
 }
