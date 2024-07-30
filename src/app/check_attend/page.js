4386b772e6011bcdc27a16c5fa43651eefd3f38c
s22025 <s22025@std.it-college.ac.jp>
2024-07-30 11:35:13 +0900

    classのidからclassName取り出したかったけどむりだから一旦コミット

--- src/app/check_attend/page.js
+++ src/app/check_attend/page.js
@@ -6,23 +6,63 @@ import {useState, useEffect} from "react";
 
 //firebase
 import {db} from "../firebase";
-import {collection, getDocs} from "firebase/firestore";
+import {collection, query, where, getDocs} from "firebase/firestore";
 
 const CheckAttend = () => {
   //state
-
+  const [attendanceData, setAttendanceData] = useState([]);
+  const [student_uid, setStudentUid] = useState('');
+  const [class_id, setClassId] = useState('');
+  const [date, setDate] = useState('');
+  const [status, setStatus] = useState('');
 
   //DBから出席情報呼び出す
-async function getAttendanceData() {
-  const querySnapshot = await getDocs(collection(db, "attendance"));
-  querySnapshot.forEach((doc) => {
-    const data = doc.data();
-    console.log(`student_uid: ${data.student_uid}, class_id: ${data.class_id}, date: ${data.date}, status: ${data.status}`);
-  });
-}
+  const getAttendanceData = async () => {
+    try {
+    const querySnapshot = await getDocs(collection(db, "attendance"));
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      setStudentUid(data.student_uid);
+      setClassId(data.class_id);
+      setDate(data.date);
+      setStatus(data.status);
+
+      //console
+      console.log(`student_uid: ${data.student_uid}, class_id: ${data.class_id}, date: ${data.date}, status: ${data.status}`);
+
+    })}catch (e) {
+      console.log(e)
+    };
+  }
+
+  //
+  const fetchData = async () => {
+    try {
+      const classQuery = query(collection(db, "class"), where("id", "==", "UJxHrzU9mWMirirlq65O"));
+      const classSnapshot = await getDocs(classQuery);
+
+      if (!classSnapshot.empty) {
+        const attendanceQuery = query(collection(db, "attendance"), where("class_id", "==", "UJxHrzU9mWMirirlq65O"));
+        const attendanceSnapshot = await getDocs(attendanceQuery);
+        const attendanceList = attendanceSnapshot.docs.map(doc => doc.data());
+
+        setAttendanceData(attendanceList);  // 取得したデータを状態にセット
+      } else {
+        console.log("No class document found.");
+      }
+
+    } catch (e) {
+     console.log(e)
+    }
+    };
+
+
+
 
   useEffect(() => {
     getAttendanceData();
+    // console.log(attendanceData)
+    // fetchData();
   }, []);
 
   return (
@@ -42,8 +82,8 @@ async function getAttendanceData() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>2024/07/15 9:30:33</td>
+          <tr>
+            <td>2024/07/15 9:30:33</td>
               <td>s00000</td>
               <td>アイカレ太郎</td>
               <td>出席</td>
