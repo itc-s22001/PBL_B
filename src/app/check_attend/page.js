a6d467b888aff800f1e7a8c0e46d77d9dacf6ec8
s22025 <s22025@std.it-college.ac.jp>
2024-07-30 10:45:14 +0900

    DBから一応データだけとれる UIには反映されない

--- src/app/check_attend/page.js
+++ src/app/check_attend/page.js
@@ -1,49 +1,30 @@
-// import s from './page.module.css';
-
-// const CheckAttend = () => {
-//   return (
-//     <div className={s.container}>
-//       <p className={s.h1}>出席状況を確認する</p>
-//       <select className={s.select}>
-//         <option>PBL</option>
-//       </select>
-//       <table className={s.table}>
-//         <thead>
-//           <tr>
-//             <th>日付</th>
-//             <th>学籍番号</th>
-//             <th>名前</th>
-//             <th>ステータス</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           <tr>
-//             <td>2024/07/15 9:30:33</td>
-//             <td>s00000</td>
-//             <td>アイカレ太郎</td>
-//             <td>出席</td>
-//           </tr>
-//           <tr>
-//             <td>2024/07/15 9:30:34</td>
-//             <td>s00001</td>
-//             <td>アイカレ花子</td>
-//             <td>欠席</td>
-//           </tr>
-//         </tbody>
-//       </table>
-//       <div className={s.returnButton}>
-//         <button>TOPに戻る</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CheckAttend;
+"use client";
 
 import Link from "next/link";
 import s from './page.module.css';
+import {useState, useEffect} from "react";
 
-const Test = () => {
+//firebase
+import {db} from "../firebase";
+import {collection, getDocs} from "firebase/firestore";
+
+const CheckAttend = () => {
+  //state
+
+
+  //DBから出席情報呼び出す
+async function getAttendanceData() {
+  const querySnapshot = await getDocs(collection(db, "attendance"));
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    console.log(`student_uid: ${data.student_uid}, class_id: ${data.class_id}, date: ${data.date}, status: ${data.status}`);
+  });
+}
+
+  useEffect(() => {
+    getAttendanceData();
+  }, []);
+
   return (
     <div className={s.container}>
       <p className={s.h1}>出席状況を確認する</p>
@@ -146,4 +127,4 @@ const Test = () => {
   );
 };
 
-export default Test;
+export default CheckAttend;
