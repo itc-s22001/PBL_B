e8bd97d5164bd4a00fb3c63af4fd903f97b723c8
itc-s22021 <s22021@std.it-college.ac.jp>
2024-08-08 12:04:41 +0900

    complete_attendからtopに戻れる

--- src/app/complete_attend/page.js
+++ src/app/complete_attend/page.js
@@ -1,22 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
 import s from './page.module.css';
 
 const Top = () => {
-	// ここに3年生が色々書きます
-	// コメントアウト
-
 	return (
 		<>
 			<p className={s.h1}>出席完了しました</p>
-		<div>
-			<p className={s.buttonContainer}>
-				<button className={`${s.button} ${s.buttonA}`}>出席画面に戻る</button>
-			</p>
-		</div>
-	</>
+			<div>
+				<p className={s.buttonContainer}>
+					<Link href="/students_login">
+						<button className={`${s.button} ${s.buttonA}`}>TOPに戻る</button>
+					</Link>
+				</p>
+			</div>
+		</>
 	);
 };
 
 // 名前付きエクスポート
-export default Top
-
+export default Top;
